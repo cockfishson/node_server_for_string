4752f9603d242b10fe991b7eea62f46cde69abfb
Soskfish <fox25055@gmail.com>
2024-11-08 19:02:37 +0300

    fix: no user :'(

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ import { authServices } from "../services/authServices.js";
 export class AuthController {
   static login(request, response) {
     const { username, password } = request.body;
-    const user = authServices.authenticateUser(username, password);
-    response.status(200).json({ success: true, user });
+    authServices.authenticateUser(username, password);
+    response.status(200).json({ success: true });
   }
 }
