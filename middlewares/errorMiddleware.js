dca1d54fb8cf7bdf4ac0cb0b8e5cc8504b0006fc
Soskfish <fox25055@gmail.com>
2024-11-08 21:02:10 +0300

    fix: error middleware stuff

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,13 +1,12 @@
 import { CustomError } from "../helpers/error_handlers/customResponseError.js";
-
-export const errorMiddleware = (error, request, response) => {
+// eslint-disable-next-line no-unused-vars
+export const errorMiddleware = (error, request, response, next) => {
   if (error instanceof CustomError) {
-    response.status(error.statusCode).json({
+    response.status(error.statusCode).send({
       success: false,
       message: error.message,
     });
   } else {
-    console.error("Unexpected error:", error);
     response.status(500).json({
       success: false,
       message: "An unexpected error occurred",
