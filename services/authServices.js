22d3a7ebdf11488db5d55aacb8a424942e520558
Soskfish <fox25055@gmail.com>
2024-11-15 21:09:46 +0300

    feat: added hashing for passwords

--- services/authServices.js
+++ services/authServices.js
@@ -2,22 +2,21 @@ import User from "../models/userModel.js";
 import { CustomError } from "../helpers/error_handlers/customResponseError.js";
 import { HttpStatus } from "../helpers/error_handlers/responseErrorCodes.js";
 import { JwtService } from "./jwtServices.js";
+import bcrypt from "bcrypt";
 
 export class authServices {
   static authenticateUser = async (username, password) => {
     const user = await User.findOne({
       where: {
         username: username.toString(),
-        password: password.toString(),
       },
     });
-    if (!user) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       throw new CustomError(
         HttpStatus.UNAUTHORIZED,
         "Invalid username or password",
       );
     }
-
     const accessToken = JwtService.sign(
       { id: user.id },
       {
@@ -49,9 +48,10 @@ export class authServices {
         username: "User with this username already exists",
       });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({
       username: username.toString(),
-      password: password.toString(),
+      password: hashedPassword,
       first_name: name.toString(),
       last_name: surname.toString(),
       age: age.toString(),
